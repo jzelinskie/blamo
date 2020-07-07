92c0f8bc1e07452a7eb1fd424921fb748ecd58d5
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2020-07-07 16:19:24 -0400

    proxy my first image!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use actix_web::http::header::HeaderValue;
 use actix_web::{get, middleware, web, App, HttpRequest, HttpResponse, HttpServer};
+
 use anyhow;
+
 use fernet::Fernet;
 
 #[get("/v1/{token}")]
@@ -9,19 +11,39 @@ async fn proxy(
     key: web::Data<fernet::Fernet>,
     token: web::Path<String>,
 ) -> HttpResponse {
+    // If the via header is from an instance of blamo, bail to avoid recursion.
+    // There's definitely a way to make this more succinct.
+    if let Some(via) = request.headers().get("Via") {
+        if via
+            .to_str()
+            .expect("http headers should be strings?")
+            .starts_with("blamo!")
+        {
+            println!("400: already via blamo!");
+            return HttpResponse::BadRequest()
+                .add_security_headers()
+                .add_cachebust_headers()
+                .finish();
+        }
+    };
+
     match key.decrypt(&token.into_inner()) {
-        Ok(url_vec) => proxy_response(request, String::from_utf8(url_vec).unwrap()),
-        Err(_) => HttpResponse::BadRequest().finish(),
+        Ok(url_vec) => proxy_response(request, String::from_utf8(url_vec).unwrap()).await,
+        Err(_) => {
+            println!("400: failed to decrypt");
+
+            HttpResponse::BadRequest()
+            .add_security_headers()
+            .add_cachebust_headers()
+            .finish()
+        }
     }
 }
 
 fn transferred_headers(request: HttpRequest) -> Vec<(String, String)> {
     vec![
-        ("Via", "blamo!"),
-        (
-            "User-Agent",
-            request.headers().get_or("User-Agent", "blamo!").as_str(),
-        ),
+        ("Via", "blamo! v0.0.1"),
+        ("User-Agent", "blamo! v0.0.1"),
         (
             "Accept",
             request.headers().get_or("Accept", "image/*").as_str(),
@@ -43,6 +65,30 @@ fn transferred_headers(request: HttpRequest) -> Vec<(String, String)> {
     .collect()
 }
 
+trait HeaderExtensions {
+    fn add_cachebust_headers(&mut self) -> &mut Self;
+    fn add_security_headers(&mut self) -> &mut Self;
+}
+
+impl HeaderExtensions for actix_web::dev::HttpResponseBuilder {
+    fn add_cachebust_headers(&mut self) -> &mut Self {
+        self.set_header(
+            "Cache-Control",
+            "no-cache, no-store, private, must-revalidate",
+        )
+    }
+
+    fn add_security_headers(&mut self) -> &mut Self {
+        self.set_header("X-Frame-Options", "deny")
+            .set_header("X-XSS-Protection", "1; mode=block")
+            .set_header("X-Content-Type-Options", "nosniff")
+            .set_header(
+                "Content-Security-Policy",
+                "default-src 'none'; img-src data:; style-src 'unsafe-inline'",
+            )
+    }
+}
+
 trait GetOr {
     fn get_or(&self, header: &str, default: &str) -> String;
 }
@@ -51,41 +97,78 @@ impl GetOr for actix_web::http::header::HeaderMap {
     fn get_or(&self, header: &str, default: &str) -> String {
         String::from(
             self.get(header)
-                .unwrap_or(&HeaderValue::from_str(default).expect("default header value should parse"))
+                .unwrap_or(
+                    &HeaderValue::from_str(default).expect("default header value should parse"),
+                )
                 .to_str()
                 .expect("header with a default should never fail .to_str()"),
         )
     }
 }
 
-fn proxy_response(request: HttpRequest, url: String) -> HttpResponse {
+async fn proxy_response(request: HttpRequest, url: String) -> HttpResponse {
     use actix_web::http::Uri;
 
     let parsed_url = match url.clone().parse::<Uri>() {
         Ok(x) => x,
-        Err(_) => return HttpResponse::BadRequest().finish(),
+        Err(_) => {
+            println!("400: failed to parse URL");
+            return HttpResponse::BadRequest()
+                .add_security_headers()
+                .add_cachebust_headers()
+                .finish()
+        }
     };
 
     let scheme = match parsed_url.scheme() {
         Some(x) => x.to_string(),
-        None => return HttpResponse::BadRequest().finish(),
+        None => {
+            println!("400: failed to parse scheme");
+            return HttpResponse::BadRequest()
+                .add_security_headers()
+                .add_cachebust_headers()
+                .finish()
+        }
     };
 
     match scheme.as_str() {
         "http" => (),
-        _ => return HttpResponse::BadRequest().finish(),
+        _ => {
+            println!("400: scheme must be http");
+            return HttpResponse::BadRequest()
+                .add_security_headers()
+                .add_cachebust_headers()
+                .finish()
+        }
     };
+
+    let client = awc::Client::default();
+    let mut proxied_request = client.get(url.as_str());
+    for (k, v) in transferred_headers(request) {
+        proxied_request = proxied_request.header(k.as_str(), v.as_str())
+    }
+    let mut response = match proxied_request.send().await {
+        Ok(x) => {
+            if x.status().is_success() {
+                x
+            } else {
+                return HttpResponse::BadGateway().finish();
+            }
+        }
+        Err(_) => return HttpResponse::BadGateway().finish(),
+    };
+
+    let body = match response.body().await {
+        Ok(x) => x,
+        Err(_) => return HttpResponse::BadGateway().finish(),
+    };
+
 
     HttpResponse::Ok()
         .header("Via", "blamo!")
-        .header("X-Frame-Options", "deny")
-        .header("X-XSS-Protection", "1; mode=block")
-        .header("X-Content-Type_options", "nosniff")
-        .header(
-            "Content-Security-Policy",
-            "default-src 'none'; img-src data:; style-src 'unsafe-inline'",
-        )
-        .body(scheme)
+        .add_security_headers()
+        .set_header("Content-Type", response.headers().get_or("Content-Type", "application/octet-stream"))
+        .body(body)
 }
 
 #[get("/v1/_metrics")]
@@ -100,20 +183,6 @@ async fn metrics() -> HttpResponse {
 
 #[actix_rt::main]
 async fn main() -> anyhow::Result<()> {
-    /*
-    let client = awc::Client::new();
-
-    let mut response = client
-        .get("https://rust-lang.org")
-        .header("User-Agent", "actix-web")
-        .send()
-        .await?;
-    println!("Response: {:?}", response);
-
-    let body = response.body().await?;
-    println!("Downloaded: {:?} bytes", body.len());
-    */
-
     let matches = clap::App::new("blamo!")
         .version("0.0.1")
         .about("securely serve trusted, insecure content")
@@ -177,15 +246,21 @@ async fn main() -> anyhow::Result<()> {
         }
 
         if let Some(matches) = matches.subcommand_matches("encrypt") {
-            let key = Fernet::new(matches.value_of("key").expect("key requires a value")).ok_or_else(|| invalid_key)?;
-            let message = matches.value_of("message").expect("message requires a value");
+            let key = Fernet::new(matches.value_of("key").expect("key requires a value"))
+                .ok_or_else(|| invalid_key)?;
+            let message = matches
+                .value_of("message")
+                .expect("message requires a value");
             println!("{}", key.encrypt(message.as_bytes()));
             return Ok(());
         }
 
         if let Some(matches) = matches.subcommand_matches("decrypt") {
-            let key = Fernet::new(matches.value_of("key").expect("key requires a value")).ok_or_else(|| invalid_key)?;
-            let ciphertext = matches.value_of("ciphertext").expect("ciphertext requires a value");
+            let key = Fernet::new(matches.value_of("key").expect("key requires a value"))
+                .ok_or_else(|| invalid_key)?;
+            let ciphertext = matches
+                .value_of("ciphertext")
+                .expect("ciphertext requires a value");
             let decrypted_message = key
                 .decrypt(ciphertext)
                 .map_err(|_| anyhow::anyhow!("failed to decrypt ciphertext"))?;
@@ -196,14 +271,18 @@ async fn main() -> anyhow::Result<()> {
 
     if let Some(matches) = matches.subcommand_matches("server") {
         if let Some(matches) = matches.subcommand_matches("run") {
-            let key_str = matches.value_of("key").expect("key requires a value").to_owned();
+            let key_str = matches
+                .value_of("key")
+                .expect("key requires a value")
+                .to_owned();
 
             // Ensure the key is valid before trying to handle any requests.
             Fernet::new(&key_str).ok_or_else(|| invalid_key)?;
 
             println!("listening on localhost:8080...");
             return HttpServer::new(move || {
-                let key = fernet::Fernet::new(&key_str.clone()).expect("key has been previously validated");
+                let key = fernet::Fernet::new(&key_str.clone())
+                    .expect("key has been previously validated");
                 App::new()
                     .wrap(middleware::Compress::default())
                     .wrap(middleware::Logger::default())
